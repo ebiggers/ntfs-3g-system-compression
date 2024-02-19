53f7bcba9c1c54d8e15e87649ed66654474885ab
Eric Biggers <ebiggers3@gmail.com>
2024-02-19 12:00:55 -0800

    Use MIT license instead of CC0
    Following the same commit from wimlib
    (https://wimlib.net/git/?p=wimlib;a=commit;h=41a9a2a95bd357a344be95d2ac3c370aadac3846),
    use the MIT license for decompress_common.c and decompress_common.h
    instead of CC0.

--- README.md
+++ README.md
@@ -47,10 +47,8 @@ The XPRESS and LZX compression formats used in system-compressed files are
 identical to the formats used in Windows Imaging (WIM) archives.  Therefore, for
 the system compression plugin I borrowed the XPRESS and LZX decompressors I had
 already written for the wimlib project (https://wimlib.net/).  I made some
-slight modifications for integration purposes.  The code in wimlib is currently
-licensed LGPLv3+, but I have relicensed the version in this plugin to GPLv2+ for
-consistency with NTFS-3G's license.  (Public domain portions remain public
-domain.)
+slight modifications for integration purposes, and I relicensed the files that
+used the LGPLv3+ license to GPLv2+ for compatibility with NTFS-3G's license.
 
 # Notices
 
--- src/decompress_common.h
+++ src/decompress_common.h
@@ -3,21 +3,28 @@
  *
  * Header for decompression code shared by multiple compression formats.
  *
- * The following copying information applies to this specific source code file:
+ * Copyright 2022 Eric Biggers
  *
- * Written in 2012-2016 by Eric Biggers <ebiggers3@gmail.com>
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
  *
- * To the extent possible under law, the author(s) have dedicated all copyright
- * and related and neighboring rights to this software to the public domain
- * worldwide via the Creative Commons Zero 1.0 Universal Public Domain
- * Dedication (the "CC0").
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
  *
- * This software is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the CC0 for more details.
- *
- * You should have received a copy of the CC0 along with this software; if not
- * see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef _DECOMPRESS_COMMON_H
